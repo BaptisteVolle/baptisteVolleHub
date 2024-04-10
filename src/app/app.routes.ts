460ae219cb1265d66b3850e3b14ca25b527bc325
BaptisteVolle <baptistevolle09@gmail.com>
2024-04-09 20:07:15 -0400

    working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,5 @@
 import { Routes } from '@angular/router';
 import { ProjectsComponent } from './projects/projects.component';
-import { WorldMapComponent } from './world-map/world-map.component';
-import { GameoflifeComponent } from './gameoflife/gameoflife.component';
-import { NonogramComponent } from './nonogram/nonogram.component';
 
 
 export const routes: Routes = [
@@ -10,23 +7,5 @@ export const routes: Routes = [
       path: '',
       component: ProjectsComponent,
       title: 'Home page'
-    },
-
-    {
-      path: 'worldmap',
-      component: WorldMapComponent,
-      title: 'World Map'
-    },
-
-    {
-      path: 'gameoflife',
-      component: GameoflifeComponent,
-      title: 'Game of life'
-    },
-
-    {
-      path: 'nonogram',
-      component: NonogramComponent,
-      title: 'Nonogram'
     },
 ];
